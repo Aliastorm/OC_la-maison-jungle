f1b7201b97437308a047216806bdad5a0c26908b
jfpons <jf.pons@gmail.com>
2022-07-06 18:54:22 +0200

    First

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Banner from './Banner';
+import Cart from './Cart';
+import '../styles/index.css';
+
+function App() {
+  return (
+    <>
+      <Banner />
+      <Cart />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+function Banner() {
+  return (
+    <div className='.lmj-banner'>
+      <h1>La Maison Jungle</h1>
+    </div>
+  );
+}
+
+export default Banner;
